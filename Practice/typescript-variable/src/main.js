04dc5451f324d87f6d11cd7808a5cc676be0ffbc
Compass <ndphong165@gmail.com>
2022-11-17 16:27:48 +0700

    done nodeJS

---
+++ Practice/typescript-variable/src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var app_module_1 = require("./app/app.module");
+(0, platform_browser_dynamic_1.platformBrowserDynamic)().bootstrapModule(app_module_1.AppModule)["catch"](function (err) { return console.error(err); });
+var width;
+var height;
+width = 10.5;
+height = 20;
+var area = width * height;
+console.log("Di\u1EC7n t\u00EDch h\u00ECnh ch\u1EEF nh\u1EADt: ".concat(area));
